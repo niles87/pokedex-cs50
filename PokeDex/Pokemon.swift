bed7697b3127d75b704827eead3ee4c60dc06042
niles87 <nbing19@gmail.com>
2020-12-02 15:16:47 -0800

    added git ignore

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Niles Bingham on 12/2/20.
+//
+
+import Foundation
---
+++ PokeDex/PokemonViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonViewController.swift
+//  PokeDex
+//
+//  Created by Niles Bingham on 12/2/20.
+//
+
+import Foundation
