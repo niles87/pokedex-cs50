28b4a2141cdf22ce47adbc8643b0bf9d9bd9745b
niles87 <nbing19@gmail.com>
2020-12-02 15:25:17 -0800

    updated ViewController

--- PokeDex/PokemonViewController.swift
+++ PokeDex/PokemonViewController.swift
@@ -5,4 +5,17 @@
 //  Created by Niles Bingham on 12/2/20.
 //
 
-import Foundation
+import UIKit
+
+class PokemonViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLabel.text = pokemon.name
+        numberLabel.text = String(format: "#%03d",pokemon.number)
+    }
+}
